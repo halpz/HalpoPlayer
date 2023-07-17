e49008861b3eac76c1ff93bacc93a9ed53719a2a
Paul Halpin <paulhalpo@gmail.com>
2023-07-17 09:41:19 +0100

    now playing CarPlay

--- HalpoPlayer/CarPlaySceneDelegate.swift
+++ HalpoPlayer/CarPlaySceneDelegate.swift
@@ -11,15 +11,9 @@ class CarPlaySceneDelegate: UIResponder, CPTemplateApplicationSceneDelegate {
 	var interfaceController: CPInterfaceController?
 	func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene, didConnect interfaceController: CPInterfaceController) {
 		self.interfaceController = interfaceController
-		// Create a list
-		let item = CPListItem(text: "title", detailText: "detail")
-		item.accessoryType = .disclosureIndicator
-		let section = CPListSection(items: [item])
-		let listTemplate = CPListTemplate(title: "Section", sections: [section])
-		// Set root
-		self.interfaceController?.setRootTemplate(listTemplate, animated: true, completion: {_, _ in })
+		let nowPlaying = CPNowPlayingTemplate.shared
+		self.interfaceController?.setRootTemplate(nowPlaying, animated: true, completion: {_, _ in })
 	}
-	// CarPlay disconnected
 	private func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene, didDisconnect interfaceController: CPInterfaceController) {
 		self.interfaceController = nil
 	}
