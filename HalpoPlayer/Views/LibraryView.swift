a0f7ef4a1319c9abba3abcace9fb6183c691f353
Paul Halpin <paulhalpo@gmail.com>
2023-07-20 11:45:20 +0100

    split up library view

--- HalpoPlayer/Views/LibraryView.swift
+++ HalpoPlayer/Views/LibraryView.swift
@@ -16,105 +16,10 @@ struct LibraryView: View {
 		if accountHolder.account != nil {
 			switch viewModel.viewType {
 			case .artists:
-				if viewModel.artists.isEmpty {
-					ProgressView()
-						.onAppear {
-							viewModel.loadContent()
-						}
-				}
-				List(viewModel.artists) { artist in
-					Button {
-						coordinator.goToArtist(artistId: artist.id)
-					} label: {
-						ArtistCell(artist: artist)
-					}
-					.listRowSeparator(.hidden)
-				}
-				.simultaneousGesture(DragGesture().onChanged({ value in
-					withAnimation {
-						MediaControlBarMinimized.shared.isCompact = true
-					}
-				}))
-				.refreshable {
-					viewModel.refresh()
-				}
-				.listStyle(.plain)
-				.searchable(text: $viewModel.searchText, prompt: "Search artists")
-				.scrollDismissesKeyboard(.immediately)
-				.navigationBarTitleDisplayMode(.inline)
-				.navigationTitle(viewModel.viewType.rawValue.capitalized)
-				.toolbar {
-					ToolbarTitleMenu {
-						Picker("Picker", selection: $viewModel.viewType) {
-							ForEach(LibraryViewType.allCases, id: \.self) { item in
-								Text(item.rawValue.capitalized)
-							}
-						}
-					}
-					ToolbarItem(placement: .navigationBarLeading) {
-						Button {
-							viewModel.goToLogin(coordinator: coordinator)
-						} label: {
-							Image(systemName: "person.circle").imageScale(.large)
-						}
-					}
-					ToolbarItem(placement: .navigationBarTrailing) {
-						Button {
-							viewModel.shuffle()
-						} label: {
-							Image(systemName: "shuffle").imageScale(.large)
-						}
-					}
-				}
+				ArtistListView(viewModel: viewModel)
 			case .albums:
-				List(viewModel.albums) { album in
-					Button {
-						viewModel.albumTapped(albumId: album.id, coordinator: coordinator)
-					} label: {
-						AlbumCell(album: Album(albumListResponse: album))
-					}
-					.listRowSeparator(.hidden)
-				}
-				.simultaneousGesture(DragGesture().onChanged({ value in
-					withAnimation {
-						MediaControlBarMinimized.shared.isCompact = true
-					}
-				}))
-				.refreshable {
-					viewModel.refresh()
-				}
-				.listStyle(.plain)
-				.searchable(text: $viewModel.searchText, prompt: "Search albums")
-				.scrollDismissesKeyboard(.immediately)
-				.navigationBarTitleDisplayMode(.inline)
-				.navigationTitle(viewModel.viewType.rawValue.capitalized)
-				.toolbar {
-					ToolbarTitleMenu {
-						Picker("Picker", selection: $viewModel.viewType) {
-							ForEach(LibraryViewType.allCases, id: \.self) { item in
-								Text(item.rawValue.capitalized)
-							}
-						}
-					}
-					ToolbarItem(placement: .navigationBarLeading) {
-						Button {
-							viewModel.goToLogin(coordinator: coordinator)
-						} label: {
-							Image(systemName: "person.circle").imageScale(.large)
-						}
-					}
-					ToolbarItem(placement: .navigationBarTrailing) {
-						Button {
-							viewModel.shuffle()
-						} label: {
-							Image(systemName: "shuffle").imageScale(.large)
-						}
-					}
-				}
+				AlbumListView(viewModel: viewModel)
 			}
-			
-			
-			
 		} else {
 			Button {
 				coordinator.goToLogin()
@@ -123,6 +28,118 @@ struct LibraryView: View {
 					.font(.largeTitle)
 			}
 			.buttonStyle(.borderedProminent)
+		}
+	}
+}
+
+struct AlbumListView: View {
+	@StateObject var viewModel: LibraryViewModel
+	@EnvironmentObject var coordinator: Coordinator
+	@EnvironmentObject var database: Database
+	@EnvironmentObject var accountHolder: AccountHolder
+	var body: some View {
+		List(viewModel.albums) { album in
+			Button {
+				viewModel.albumTapped(albumId: album.id, coordinator: coordinator)
+			} label: {
+				AlbumCell(album: Album(albumListResponse: album))
+			}
+			.listRowSeparator(.hidden)
+		}
+		.simultaneousGesture(DragGesture().onChanged({ value in
+			withAnimation {
+				MediaControlBarMinimized.shared.isCompact = true
+			}
+		}))
+		.refreshable {
+			viewModel.refresh()
+		}
+		.listStyle(.plain)
+		.searchable(text: $viewModel.searchText, prompt: "Search albums")
+		.scrollDismissesKeyboard(.immediately)
+		.navigationBarTitleDisplayMode(.inline)
+		.navigationTitle(viewModel.viewType.rawValue.capitalized)
+		.toolbar {
+			ToolbarTitleMenu {
+				Picker("Picker", selection: $viewModel.viewType) {
+					ForEach(LibraryViewType.allCases, id: \.self) { item in
+						Text(item.rawValue.capitalized)
+					}
+				}
+			}
+			ToolbarItem(placement: .navigationBarLeading) {
+				Button {
+					viewModel.goToLogin(coordinator: coordinator)
+				} label: {
+					Image(systemName: "person.circle").imageScale(.large)
+				}
+			}
+			ToolbarItem(placement: .navigationBarTrailing) {
+				Button {
+					viewModel.shuffle()
+				} label: {
+					Image(systemName: "shuffle").imageScale(.large)
+				}
+			}
+		}
+	}
+}
+
+struct ArtistListView: View {
+	@StateObject var viewModel: LibraryViewModel
+	@EnvironmentObject var coordinator: Coordinator
+	@EnvironmentObject var database: Database
+	@EnvironmentObject var accountHolder: AccountHolder
+	var body: some View {
+		if viewModel.artists.isEmpty {
+			ProgressView()
+				.onAppear {
+					viewModel.loadContent()
+				}
+		}
+		List(viewModel.artists) { artist in
+			Button {
+				coordinator.goToArtist(artistId: artist.id)
+			} label: {
+				ArtistCell(artist: artist)
+			}
+			.listRowSeparator(.hidden)
+		}
+		.simultaneousGesture(DragGesture().onChanged({ value in
+			withAnimation {
+				MediaControlBarMinimized.shared.isCompact = true
+			}
+		}))
+		.refreshable {
+			viewModel.refresh()
+		}
+		.listStyle(.plain)
+		.searchable(text: $viewModel.searchText, prompt: "Search artists")
+		.scrollDismissesKeyboard(.immediately)
+		.navigationBarTitleDisplayMode(.inline)
+		.navigationTitle(viewModel.viewType.rawValue.capitalized)
+		.toolbar {
+			ToolbarTitleMenu {
+				Picker("Picker", selection: $viewModel.viewType) {
+					ForEach(LibraryViewType.allCases, id: \.self) { item in
+						Text(item.rawValue.capitalized)
+					}
+				}
+			}
+			ToolbarItem(placement: .navigationBarLeading) {
+				Button {
+					viewModel.goToLogin(coordinator: coordinator)
+				} label: {
+					Image(systemName: "person.circle").imageScale(.large)
+				}
+			}
+			ToolbarItem(placement: .navigationBarTrailing) {
+				Button {
+					viewModel.shuffle()
+				} label: {
+					Image(systemName: "shuffle").imageScale(.large)
+				}
+			}
 		}
 	}
 }
